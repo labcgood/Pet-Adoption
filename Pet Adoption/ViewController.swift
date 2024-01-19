fc98c882133157f3fa8a9e1a87c836a9549e8274
Rabbit <labcgood44@gmail.com>
2024-01-19 16:31:43 +0800

    Initial Commit

---
+++ Pet Adoption/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pet Adoption
+//
+//  Created by Labe on 2024/1/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
